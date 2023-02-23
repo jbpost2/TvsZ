012ad84e7985a40ad1dc12f905347b27d9e6e51f
Justin Post <jbpost2@ncsu.edu>
2023-02-23 10:04:35 -0500

    Add files via upload

---
+++ server.R
@@ -0,0 +1,87 @@
+library(shiny)
+library(markdown)
+
+shinyServer(function(input, output, session) {
+
+  #Plot of Standard Normal Distribution with t
+  output$distPlot<-renderPlot({
+
+    #get inputs
+    df <- input$df
+
+    #plotting sequence
+    x<-seq(from = -6, to = 6, length = 50000)
+    
+
+    ##Graph of Z distribution
+    plot(x = x, y = dnorm(x, mean = 0, sd = 1), type = 'l', 
+         col = "blue", xaxt = "n", ylab = "PDF", 
+         lwd = 3, xlab = "z or t Values", main = paste("Distribution of Z and \n t with", df, "Degrees of Freedom"))
+    axis(side=1)
+    
+    ##Overlay t distribution
+    lines(x = x, y = dt(x, df = df), type = "l", col = "Red", lwd = 3)
+    
+    legend(x = 2.75, y = 0.375, legend = c("Standard Normal", paste("t with", df, "df")), lty = 1, lwd = 3, col = c("blue", "red"))
+    if(!input$prob) { 
+      #add nothing
+    } else { #add shading
+      if(input$probType == "Less"){
+        shortseq <- seq(from = -6, to = input$probL, length = 10000)
+        polygon(c(shortseq, rev(shortseq)), c(rep(0, length(shortseq)), rev(dnorm(shortseq))),
+                col = rgb(0.3, 0.1, 0.5, 0.3))
+        polygon(c(shortseq, rev(shortseq)), c(rep(0, length(shortseq)), rev(dt(shortseq, df))),
+                col = rgb(0.7, 0.1, 0.5, 0.3))
+      } else if(input$probType == "Greater") {
+        shortseq <- seq(from = input$probU, to = 6, length = 10000)
+        polygon(c(shortseq, rev(shortseq)), c(rep(0, length(shortseq)), rev(dnorm(shortseq))),
+                col = rgb(0.3, 0.1, 0.5, 0.3))
+        polygon(c(shortseq, rev(shortseq)), c(rep(0, length(shortseq)), rev(dt(shortseq, df))),
+                col = rgb(0.7, 0.1, 0.5, 0.3))
+      } else {
+        shortseq <- seq(from = input$probLB, to = input$probUB, length = 10000)
+        polygon(c(shortseq, rev(shortseq)), c(rep(0, length(shortseq)), rev(dnorm(shortseq))),
+                col = rgb(0.3, 0.1, 0.5, 0.3))
+        polygon(c(shortseq, rev(shortseq)), c(rep(0, length(shortseq)), rev(dt(shortseq, df))),
+                col = rgb(0.7, 0.1, 0.5, 0.3))
+      }
+    }
+  
+  })
+  
+
+  #Latex of conversion
+  output$text<-renderUI({
+
+    df <- input$df 
+    
+    if(!input$prob){
+      NULL
+    } else {
+      if(input$probType == "Less"){
+        tprob <- round(pt(input$probL, df), 6)
+        zprob <- round(pnorm(input$probL), 6)
+        
+        #output
+        t <- paste("P\\left(Z\\leq", input$probL, "\\right) = ", zprob,
+              "\n P\\left(T", df, "df \\leq", input$probL, "\\right) = ", tprob)
+      } else if(input$probType == "Greater") {
+        tprob <- round(pt(input$probU, df, lower.tail = FALSE), 6)
+        zprob <- round(pnorm(input$probU, lower.tail = FALSE), 6)
+        
+        #output
+        t <- paste("P\\left(Z\\geq", input$probU, "\\right) = ", zprob,
+          "\n P\\left(T", df, "df \\geq", input$probU, "\\right) = ", tprob)
+      } else {
+        tprob <- round(pt(input$probUB, df) - pt(input$probLB, df), 6)
+        zprob <- round(pnorm(input$probUB) - pnorm(input$probLB), 6)
+        
+        #output
+        t <- paste("P\\left(", input$probLB, "\\leq Z\\leq", input$probUB, "\\right) = ", zprob, 
+              "\n P\\left(", input$probLB, "\\leq T", df, "df \\leq", input$probUB, "\\right) = ", tprob)
+      }
+    }
+    tagList(withMathJax(), t)
+  })
+  
+})    
---
+++ ui.R
@@ -0,0 +1,93 @@
+###########################################################################
+##R Shiny App to visualize t vs Z scores 
+###########################################################################
+  
+library(shiny)
+library(shinydashboard)
+
+# Define UI for application that displays an about page and the app itself
+
+dashboardPage(skin="red",
+  #add title
+  dashboardHeader(title="Comparing Standard Normal Random Variables & the (central) t Distribution",titleWidth=750),
+              
+  #define sidebar items
+  dashboardSidebar(sidebarMenu(
+    menuItem("About", tabName = "about", icon = icon("archive")),
+    menuItem("Application", tabName = "app", icon = icon("laptop"))
+  )),
+              
+  #define the body of the app
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(tabName = "about",
+        fluidRow(
+          #add in latex functionality if needed
+          withMathJax(),
+                
+          #two columns for each of the two items
+          column(6,
+            #Description of App
+            h1("What does this app do?"),
+            #box to contain description
+            box(background="red",width=12,
+              h4("This application visualizes the standard normal distribution and t distributions with user specified degrees of freedom."),
+              h4("Probabilities can be calculated from each distribution and compared.")
+            )
+          ),
+                
+          column(6,
+            #How to use the app
+            h1("How to use the app?"),
+            #box to contain description
+            box(background="red",width=12,
+              h4("The controls for the app are located to the left and the visualization and information are available on the right."),
+              h4("The box on the top left has a slider that allows the user to change the degrees of freedom for the t-distribution.  There is a play button on the right which will produce an animation that varies the degrees of freedom from the current degrees of freedom to 50."),
+              h4("Below the slider there is a check box.  If checked, probabilities can be specified, the graphs will be shaded appropriately, and output will be created below the graph on the right displaying the numeric values for the probabilities rounded to six decimal places."),
+              h4("The radio buttons that appear can be used to select a less than, between, or greater than probability and boxes show up allowing you to specify the values of interest.")
+              )
+          )
+        )
+      ),
+      
+      #actual app layout      
+      tabItem(tabName = "app",        
+        fluidRow(
+          column(4,
+            box(width = 12, background = "red",
+              sliderInput("df",h3('Select the Degrees of Freedom for your t-distribution'), min = 1, max = 50, step = 1, value = 1, animate = TRUE)
+            ),
+            box(width=12,background="red",title=h3("Comparison of Probabilities?"),
+                checkboxInput("prob", "Find a probability"),
+                conditionalPanel('input.prob', 
+                                 radioButtons("probType", label = "Type of Calculation", list("Less than (or equal)" = "Less", "Between" = "Between", "Greater than (or equal)" = "Greater")),
+                                 conditionalPanel('input.probType == "Less"',
+                                                 numericInput("probL", "Lower limit", value = 0, step = 0.1)),
+                                 conditionalPanel('input.probType == "Between"',
+                                                 numericInput("probLB", "Lower limit", value = -1, step = 0.1),
+                                                 numericInput("probUB", "Upper limit", value = 1, step = 0.1)),
+                                 conditionalPanel('input.probType == "Greater"',
+                                                 numericInput("probU", "Upper limit", value = 0, step = 0.1))
+                )
+            )
+          ),
+      
+          #Show a plot of the distribution  
+          column(8,
+            fluidRow(
+              box(width = 8, plotOutput("distPlot"), height = '100%')
+            ), 
+            fluidRow(
+              conditionalPanel("input.prob", h2("Probability"),
+                               box(width = 8, background = "red", uiOutput("text"))
+              )
+            )
+          )
+        )
+      )
+    )
+  )
+)
+
+
